ff0d3f295fee1a549b8599f45a820118d0602021
laurqsr <santosribeiro.laura@gmail.com>
2024-11-06 08:27:36 -0300

    fix connection

--- src/databases/config.js
+++ src/databases/config.js
@@ -1,7 +1,8 @@
 import { createConnection } from 'mysql2/promise';
 
 export const connection = createConnection({
-  host: 'mysql.infocimol.com.br',
+  //host: 'mysql.infocimol.com.br',
+  host: '44.227.217.144',
   user: 'infocimol21',
   database: 'infocimol21',
   port: '3306',
